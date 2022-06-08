c09846cb0d9266cfef6bfbc0c90db9a3e4e6155e
Bhavesh <maheshwaribhavesh95863@gmail.com>
2022-06-08 12:09:52 +0000

    feat; api for atw app

--- wms/atw_integration/api.py
+++ wms/atw_integration/api.py
@@ -50,7 +50,7 @@ def get_portion_list(employee,batch_no):
 		warehouse = frappe.db.get_value("Warehouse",{"employee":employee},"name")
 		if not warehouse:
 			return gen_response(200, "No any linked warehouse with loggedin user",[])
-		portions = frappe.db.sql("""select distinct portion_no as 'portion_no',welding as 'welding',welding_tolerance 'tolerance' from `tabPortion Traceability` where target_warehouse=%s and batch_no=%s""",(warehouse,batch_no),as_dict=1)
+		portions = frappe.db.sql("""select distinct portion_no as 'portion_no',welding as 'welding',welding_tolerance 'tolerance',welding as 'welding_sync',welding_tolerance 'tolerance_tolerance' from `tabPortion Traceability` where target_warehouse=%s and batch_no=%s""",(warehouse,batch_no),as_dict=1)
 		return gen_response(200,"Portions List Get Successfully",portions)
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback())
@@ -60,6 +60,8 @@ def get_portion_list(employee,batch_no):
 def crate_welding(**kwargs):
 	try:
 		data = frappe._dict(kwargs)
+		if frappe.db.exists("ATW Welding",{"batch_no":data.get("batch_no"),"portion": data.get("portion")}):
+			return gen_response(500,"Welding already created")
 		doc = frappe.new_doc("ATW Welding")
 		doc.actual_welder = data.get("employee")
 		doc.date_of_welding = data.get("date_of_welding")
@@ -69,8 +71,96 @@ def crate_welding(**kwargs):
 		doc.lhrh = data.get("lhrh")
 		doc.batch_no = data.get("batch_no")
 		doc.portion = data.get("portion")
-		doc.insert(ignore_permissions = True)
+		doc.weld_no = data.get("weld_no")
+		doc.auto_gen_id = data.get("auto_gen_id")
+		doc.welder_code = frappe.db.get_value("Employee",data.get("employee"),"welder_no")
+		res = doc.insert(ignore_permissions = True)
+		file_url = upload_photo(data.get("photo_content"),data.get("file_name"),"portion_slip_photo",res.doctype,res.name)
+		frappe.db.set_value("Portion Traceability",data.get("portion"),"portion_slip_photo_link",file_url)
 		return gen_response(200,"Welding created Successfully")
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback())
+		gen_response(500, cstr(e))
+
+
+@frappe.whitelist()
+def create_tolerance(**kwargs):
+	try:
+		data = frappe._dict(kwargs)
+		if not data.get("weld_no"):
+			return gen_response(500, "Weld No Must Required")
+		if not frappe.db.exists("ATW Welding",data.get("weld_no")):
+			return gen_response(500, "Welding No Incorrect")
+		doc = frappe.get_doc("ATW Welding",data.get("weld_no"))
+		doc.tolerance_1_mtr_vertical = data.get("tolerance_1_mtr_vertical")
+		doc.tolerance_10cm_vertical = data.get("tolerance_10cm_vertical")
+		doc.tolerance_1_mtr_horizontal = data.get("tolerance_1_mtr_horizontal")
+		doc.tolerance_10cm_horizontal = data.get("tolerance_10cm_horizontal")
+		doc.location_of_weld = cstr(data.get('latitude')) + ',' + cstr(data.get('longitude'))
+		doc.tolerance = 1
+		res = doc.save(ignore_permissions = True)
+		file_url = upload_photo(data.get("photo_content"),data.get("file_name"),"photo_of_weld",res.doctype,res.name)
+		frappe.db.set_value("Portion Traceability",doc.get("portion"),"photo_of_weld",file_url)
+		return gen_response(200,"Tolerance updated successfully")
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback())
+		gen_response(500, cstr(e))
+
+
+@frappe.whitelist()
+def upload_photo(content,filename,fieldname,attached_to_doctype,attached_to_name):
+	import base64
+
+	ret = frappe.get_doc({
+		"doctype": "File",
+		"attached_to_name": attached_to_name,
+		"attached_to_doctype": attached_to_doctype,
+		"attached_to_field": fieldname,
+		"file_name": filename,
+		"is_private": 0,
+		"content": content,
+		"decode": True
+	})
+	ret.save()
+	frappe.db.commit()
+	frappe.db.set_value(attached_to_doctype,attached_to_name,fieldname,ret.file_url)
+	return ret.file_url
+	# return ret
+
+@frappe.whitelist()
+def get_pwi_list():
+	try:
+		pwi_list = (frappe.get_meta("ATW Welding").get_field("pwi").options or "").split("\n")
+		return gen_response(200,"PWI Get successfully",pwi_list)
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback())
+		gen_response(500, cstr(e))
+
+@frappe.whitelist()
+def get_aenxen_list():
+	try:
+		pwi_list = (frappe.get_meta("ATW Welding").get_field("aenxen").options or "").split("\n")
+		return gen_response(200,"PWI Get successfully",pwi_list)
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback())
+		gen_response(500, cstr(e))	
+
+@frappe.whitelist()
+def get_welding_no(employee_id):
+	try:
+		welding_list = frappe.get_all("ATW Welding",filters={"actual_welder":employee_id},fields=["name","tolerance"])
+		return gen_response(200,"Welding Details Get Successfully",welding_list)
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback())
+		gen_response(500, cstr(e))
+
+@frappe.whitelist()
+def get_settings(employee):
+	try:
+		settings = frappe.get_doc("ATW App Settings","ATW App Settings")
+		return gen_response(200,"Settings Get Successfully",dict(
+			sync_data = settings.get("sync_data")
+		))
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback())
 		gen_response(500, cstr(e))
--- wms/atw_integration/doctype/atw_welding/atw_welding.py
+++ wms/atw_integration/doctype/atw_welding/atw_welding.py
@@ -3,8 +3,39 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate,today,cstr
 
 class ATWWelding(Document):
-	pass
+	def autoname(self):
+		if self.get('auto_gen_id'):
+			self.name = self.get('auto_gen_id')
+		else:
+			self.name = cstr(getdate(today()).month) + '-' + cstr(getdate(today()).year) + '-' + self.welder_code + '-' + self.weld_no
+
+	def after_insert(self):
+		portion_doc = frappe.get_doc("Portion Traceability",self.portion)
+		portion_doc.actual_welder = self.actual_welder
+		portion_doc.weld_no = self.weld_no
+		portion_doc.date_of_welding = self.date_of_welding
+		portion_doc.pwi = self.pwi
+		portion_doc.aen_xen = self.aenxen
+		portion_doc.km_tp = self.kmtp
+		portion_doc.lh_rh = self.lhrh
+		portion_doc.portion_slip_photo_link = self.portion_slip_photo
+		portion_doc.welding = 1
+		portion_doc.save(ignore_permissions = True)
+
+	def on_update(self):
+		if self.tolerance:
+			portion_doc = frappe.get_doc("Portion Traceability",self.portion)
+			portion_doc.tolerance_1_mtr_vertical = self.get("tolerance_1_mtr_vertical")
+			portion_doc.tolerance_10cm_vertical = self.get("tolerance_10cm_vertical")
+			portion_doc.tolerance_1_mtr_horizontal = self.get("tolerance_1_mtr_horizontal")
+			portion_doc.tolerance_10cm_horizontal = self.get("tolerance_10cm_horizontal")
+			portion_doc.location_of_weld = self.location_of_weld
+			portion_doc.photo_of_weld = self.photo_of_weld
+			portion_doc.welding_tolerance = 1
+			portion_doc.save(ignore_permissions = True)
+	
